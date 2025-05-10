643156d6e6a8c1ec41f162d46789e116906d12a1
Julio Vettori <julio@vettori.dev>
2025-05-10 16:58:34 -0300

    fix: ensuring that staff.controller functions always return void

--- src/controllers/staff.controller.ts
+++ src/controllers/staff.controller.ts
@@ -1,40 +1,37 @@
-import { StaffService } from "../services/staff.service.js";
-import { Request, Response } from 'express';
-import { createStaffSchema, staffSchema } from "../schemas/staff.schema.js";
+import { StaffService } from '../services/staff.service.js'
+import { Request, Response } from 'express'
+import { createStaffSchema, staffSchema } from '../schemas/staff.schema.js'
 
-const listStaffController = async (
-    request: Request, response: Response
-) => {
-    try {
-        const staff_list = await StaffService.list();
-        response.status(200).json(staff_list);
-    } catch (error) {
-        console.error(error);
-        response.status(500).json({ error: 'Internal Server Error' });
-    }
+const listStaffController = async (request: Request, response: Response) => {
+  try {
+    const staff_list = await StaffService.list()
+    response.status(200).json(staff_list)
+  } catch (error) {
+    console.error(error)
+    response.status(500).json({ error: 'Internal Server Error' })
+  }
 }
 
-const createStaffController = async (
-    request: Request, response: Response
-) => {
-    try {
-        if(!request.body) throw new Error('Request body is empty');
-        const { email, password } = request.body;
+const createStaffController = async (request: Request, response: Response) => {
+  try {
+    if (!request.body) throw new Error('Request body is empty')
+    const { email, password } = request.body
 
-        if(!email || !password) {
-            return response.status(400).json({ error: 'Email and password are required' });
-        }
-        const staff = await StaffService.create({ email, password });
-        response.status(201).json(staff);
-        return;
-    } catch (error) {
-        console.error(error);
-        response.status(500).json({ error: 'Internal Server Error' });
-        return;
+    if (!email || !password) {
+      response.status(400).json({ error: 'Email and password are required' })
+      return
     }
+    const staff = await StaffService.create({ email, password })
+    response.status(201).json(staff)
+    return
+  } catch (error) {
+    console.error(error)
+    response.status(500).json({ error: 'Internal Server Error' })
+    return
+  }
 }
 
 export const StaffController = {
-    create: createStaffController,
-    list: listStaffController
+  create: createStaffController,
+  list: listStaffController,
 }
